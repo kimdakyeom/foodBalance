18ab3f02abb589981971d0414816e0715b6baa36
kimda <kdk9912@naver.com>
2022-02-06 20:46:44 +0900

    파베 정보 불러오기

--- app/src/main/java/com/dkkim/anew/Fragment/DietFragment.kt
+++ app/src/main/java/com/dkkim/anew/Fragment/DietFragment.kt
@@ -14,6 +14,7 @@ import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.dkkim.anew.Model.FoodInfo
+import com.dkkim.anew.Model.UserAccount
 import com.dkkim.anew.R
 import com.dkkim.anew.RecyclerView.SettingItemAdapter
 import com.dkkim.anew.databinding.FragmentDietBinding
@@ -23,10 +24,12 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.firestore.auth.User
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.fragment_diet.*
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.collections.ArrayList
 import com.dkkim.anew.RecyclerView.DietAdapter as DietAdapter
 import kotlin.collections.arrayListOf as arrayList
 
@@ -35,21 +38,46 @@ class DietFragment : Fragment() {
     private val binding get() = mBinding!!
     private lateinit var firebaseAuth: FirebaseAuth
 
+    private val dietList = arrayList<FoodInfo>()
+    lateinit var dietRecyclerView: RecyclerView
+
+    var user = FoodInfo(null, null, null, null, null, null, null)
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         mBinding = FragmentDietBinding.inflate(inflater, container, false)
+        val mDatabase = FirebaseDatabase.getInstance().getReference("UserAccount")
 
-        val dietList = arrayList(
-            FoodInfo("김치", "한성", 34.0, 24.0, 35.0, 36.0, 45.5),
-            FoodInfo("된장", "한성", 34.0, 24.0, 35.0, 36.0, 45.5)
+        val dietAdpater = DietAdapter(dietList)
+        val layout = LinearLayoutManager(context)
 
-        )
+
+        mDatabase.child(Firebase.auth.currentUser?.uid.toString()).addValueEventListener(object : ValueEventListener {
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                dietList.clear()
+                for (snapshotChild in snapshot.children) {
+                    val getData = snapshotChild.child("Account").getValue(FoodInfo::class.java)
+                    dietList.add(getData!!)
+                }
+                dietAdpater.notifyDataSetChanged()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.w("DietFragment", "Failed to read value.", error.toException())
+            }
+        })
+
+        dietRecyclerView.layoutManager = layout
+        dietRecyclerView.adapter = dietAdpater
         binding.dietRecyclerView.layoutManager =
             LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
         binding.dietRecyclerView.setHasFixedSize(true)
-        binding.dietRecyclerView.adapter = DietAdapter(dietList)
+
 
         return binding.root
     }
