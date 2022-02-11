2ff8bb6210ce135f63b938bb26e1462fdd1057fa
kimda <kdk9912@naver.com>
2022-02-11 12:32:09 +0900

    join 조건

--- app/src/main/java/com/dkkim/anew/Activity/JoinActivity.kt
+++ app/src/main/java/com/dkkim/anew/Activity/JoinActivity.kt
@@ -12,12 +12,17 @@ import com.dkkim.anew.Model.UserAccount
 import com.dkkim.anew.databinding.ActivityJoinBinding
 
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
 import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.*
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.fragment_calendar_result.*
+import java.lang.Exception
 import java.text.SimpleDateFormat
 import java.time.LocalDate
 import java.util.*
+import android.util.Log.d as Log
 
 
 class JoinActivity : AppCompatActivity() {
@@ -43,6 +48,7 @@ class JoinActivity : AppCompatActivity() {
         }
 
         firebaseAuth = FirebaseAuth.getInstance() // FirebaseAuth 인스턴스 초기화
+
 
         // 가입버튼 -> firebase에 데이터 저장
         binding.joinBtn.setOnClickListener {
@@ -82,9 +88,12 @@ class JoinActivity : AppCompatActivity() {
                                 weight
                             ) // UserAccount 모댈 변수
 
-                            val db: FirebaseDatabase = FirebaseDatabase.getInstance() // FirebaseDatabase 인스턴스 초기화
-                            val reference: DatabaseReference = db.getReference("UserAccount") // DatabaseReference를 매개체 삼아 읽기/쓰기
-                            reference.child(uid.toString()).child("account").setValue(account) // reference에서 하위 값의 uid를 account에 즉시 값 변경
+                            val db: FirebaseDatabase =
+                                FirebaseDatabase.getInstance() // FirebaseDatabase 인스턴스 초기화
+                            val reference: DatabaseReference =
+                                db.getReference("UserAccount") // DatabaseReference를 매개체 삼아 읽기/쓰기
+                            reference.child(uid.toString()).child("account")
+                                .setValue(account) // reference에서 하위 값의 uid를 account에 즉시 값 변경
 
                             // 가입성공시 join액티비티 빠져나와 login액티비티로
                             val intent = Intent(this@JoinActivity, LoginActivity::class.java)
@@ -93,12 +102,13 @@ class JoinActivity : AppCompatActivity() {
                             Toast.makeText(this@JoinActivity, "회원가입이 완료되었습니다.", Toast.LENGTH_SHORT)
                                 .show()
 
-                        } else { // 가입 실패시
-                            Toast.makeText(
-                                this@JoinActivity,
-                                "가입에 실패했습니다.\n다시 시도해주세요", Toast.LENGTH_SHORT
-                            ).show()
-                            Log.w(TAG, "signInWithEmail:failure", task.exception) // 예외 log에 찍기
+                        } else {
+                            if (task.exception is FirebaseAuthUserCollisionException) { // 가입 실패시
+                                Toast.makeText(
+                                    this@JoinActivity,
+                                    "아이디가 중복됩니다.", Toast.LENGTH_SHORT
+                                ).show()
+                            }
                         }
                     }
             } else { // 비밀번호란과 비밀번호 확인란이 일치하지 않을 때
