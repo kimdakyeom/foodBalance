75b62cbc34f13a5d6bd3c2ea77831bb3d16aea27
kimda <kdk9912@naver.com>
2022-03-22 14:36:21 +0900

    again..

--- app/src/main/java/com/dkkim/anew/Fragment/DietFragment.kt
+++ app/src/main/java/com/dkkim/anew/Fragment/DietFragment.kt
@@ -15,6 +15,7 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.annotation.RequiresApi
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.databinding.DataBindingUtil.setContentView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DividerItemDecoration
@@ -44,6 +45,7 @@ import kotlin.collections.ArrayList
 import com.dkkim.anew.RecyclerView.DietAdapter as DietAdapter
 import kotlin.collections.arrayListOf as arrayList
 
+
 class DietFragment : Fragment() {
     lateinit var mBinding: FragmentDietBinding
     private val binding get() = mBinding!!
@@ -52,13 +54,9 @@ class DietFragment : Fragment() {
     private val dietList = arrayList<FoodInfo>()
     lateinit var dietRecyclerView: RecyclerView
 
-    private lateinit var dateTextView: TextView
-    private lateinit var leftImageView: ImageView
-    private lateinit var rightImageView: ImageView
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View? {
         mBinding = FragmentDietBinding.inflate(inflater, container, false)
         val mDatabase = FirebaseDatabase.getInstance().getReference("UserAccount")
@@ -66,7 +64,30 @@ class DietFragment : Fragment() {
         val dietAdpater = DietAdapter(dietList)
 
         val today = System.currentTimeMillis()
-        val todayDate = SimpleDateFormat("yyyy/M/d", Locale.KOREAN).format(today)
+        val simpleDateFormat = SimpleDateFormat("yyyy-M-d", Locale.KOREAN).format(today)
+        val TodayDate = SimpleDateFormat("yyyy.M.d", Locale.KOREAN).format(today)
+
+        binding.btnDate.text = TodayDate
+
+        binding.btnDate.setOnClickListener {
+            val cal = Calendar.getInstance()
+            val dateSetListener =
+                DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+                    btn_date.text = "${year}.${month + 1}.${dayOfMonth}"
+                }
+            DatePickerDialog(requireContext(),
+                dateSetListener,
+                cal.get(Calendar.YEAR),
+                cal.get(Calendar.MONTH),
+                cal.get(Calendar.DAY_OF_MONTH)).show()
+        }
+
+        binding.btnLeft.setOnClickListener {
+            val day = Calendar.getInstance()
+            day.add(Calendar.DATE, -1)
+            val beforeDay = SimpleDateFormat("yyyy.M.d", Locale.KOREAN).format(day.time)
+            System.out.println(beforeDay)
+        }
 
         binding.btnAdd.setOnClickListener {
             parentFragmentManager
@@ -76,23 +97,24 @@ class DietFragment : Fragment() {
                 .commit()
         }
 
-        mDatabase.child(Firebase.auth.currentUser?.uid.toString()).child(todayDate).addValueEventListener(object : ValueEventListener {
+        mDatabase.child(Firebase.auth.currentUser?.uid.toString()).child(simpleDateFormat)
+            .addValueEventListener(object : ValueEventListener {
 
-            override fun onDataChange(snapshot: DataSnapshot) {
+                override fun onDataChange(snapshot: DataSnapshot) {
 
-                dietList.clear()
-                for (snapshotChild in snapshot.children) {
-                    val getData = snapshotChild.getValue(FoodInfo::class.java)
+                    dietList.clear()
+                    for (snapshotChild in snapshot.children) {
+                        val getData = snapshotChild.getValue(FoodInfo::class.java)
 
-                    dietList.add(getData!!)
+                        dietList.add(getData!!)
+                    }
+                    dietAdpater.notifyDataSetChanged()
                 }
-                dietAdpater.notifyDataSetChanged()
-            }
 
-            override fun onCancelled(error: DatabaseError) {
-                Log.w("DietFragment", "Failed to read value.", error.toException())
-            }
-        })
+                override fun onCancelled(error: DatabaseError) {
+                    Log.w("DietFragment", "Failed to read value.", error.toException())
+                }
+            })
 
         binding.dietRecyclerView.layoutManager =
             LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
@@ -102,62 +124,4 @@ class DietFragment : Fragment() {
 
         return binding.root
     }
-
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        val today = System.currentTimeMillis()
-        val todayDate = SimpleDateFormat("yyyy/M/d", Locale.KOREAN).format(today)
-        val cal = Calendar.getInstance()
-
-        dateTextView = view.findViewById(R.id.btn_date)
-        leftImageView = view.findViewById(R.id.btn_left)
-        rightImageView = view.findViewById(R.id.btn_right)
-
-        var date: Date? = null
-
-        dateTextView.text = todayDate
-
-        class OnClickListener : View.OnClickListener {
-            @RequiresApi(Build.VERSION_CODES.O)
-            override fun onClick(v: View?) {
-
-                var str_getDate: String = dateTextView.text.toString()
-                var date: Date? = null
-
-                when (view.id) {
-                    R.id.btn_date -> {
-                        val dateSetListener = DatePickerDialog.OnDateSetListener{ view, year, month, dayOfmonth ->
-                            cal.set(Calendar.YEAR, year)
-                            cal.set(Calendar.MONTH, month)
-                            cal.set(Calendar.DAY_OF_MONTH, dayOfmonth)
-
-                            val calDateFormat = SimpleDateFormat("yyyy/M/d", Locale.KOREAN)
-                            btn_date.text = calDateFormat.format(cal.time)
-                        }
-
-                        DatePickerDialog(requireContext(), dateSetListener, cal.get(Calendar.YEAR),cal.get(Calendar.MONTH),
-                                cal.get(Calendar.DAY_OF_MONTH)).show()
-                    }
-
-                    R.id.btn_left -> {
-                        // val date = LocalDate.parse(str_getDate, DateTimeFormatter.ISO_DATE)
-                        val cal = Calendar.getInstance()
-                        val df: DateFormat = SimpleDateFormat("yyyy/M/d")
-                        date = df.parse(str_getDate)
-                        cal.time = date
-                        cal.add(Calendar.DATE, -1)
-
-                        val beforeDay = SimpleDateFormat("yyyy/M/d", Locale.KOREAN)
-
-                        btn_date.text = beforeDay.format(cal.time)
-                    }
-                }
-            }
-
-        }
-
-    }
-
 }
